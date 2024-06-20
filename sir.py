ea34898a80b659adad1bf5d6859a4721eb77fd33
Niels Raunkjær Holm <nielsraunkjaer@gmail.com>
2024-06-20 14:59:44 +0200

    Implement argument parsing

--- sir.py
+++ sir.py
@@ -34,9 +34,25 @@ if __name__ == '__main__':
     from seaborn import set_style
     set_style('darkgrid')
 
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default="sir", choices=["sir"], help='which model to use for epidemic simulation (default: %(default)s)')
+    parser.add_argument('--S0', type=int, default=1, help='initial condition for [S]usceptible (default: %(default)s)')
+    parser.add_argument('--I0', type=int, default=1, help='initial condition for [I]nfected (default: %(default)s)')
+    parser.add_argument('--R0', type=int, default=1, help='initial condition for [R]ecovered (default: %(default)s)')
+    parser.add_argument('--beta', type=float, default=1e-3, help='infection likelihood parameter (default: %(default)s)')
+    parser.add_argument('--gamma', type=float, default=1e-3, help='recovery rate parameter (default: %(default)s)')
+    args = parser.parse_args()
+
+    print('\n# Options')
+    for key, value in sorted(vars(args).items()):
+        print(key, '=', value)
+    print("")
+
+
     # Initial conditions
     S0, I0, R0 = 990, 10, 0
-    y0 = torch.tensor([S0, I0, R0], dtype=torch.float32)#.unsqueeze(1)
+    y0 = torch.tensor([S0, I0, R0], dtype=torch.float32)
 
     # SIR model
     beta = 0.002
